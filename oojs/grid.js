adfe8f310c93b92fb5a9a21c80255d84306b1adc
Ambrosiano <119751@win.lanl.gov>
2018-05-22 12:07:48 -0600

    Successful experiments with Grid object and Plotly.js library.

--- oojs/grid.js
+++ oojs/grid.js
@@ -1,8 +1,23 @@
-/**
-    Defines a set of prototypes for regular grids.
+/*
+    The class defines a set of prototypes for regular grids.
+    The origin of the grid is in the lower left of the matrix.
+    The first index in the setter/getter is the column index
+    (column-major order).
 */
 function Grid(nx,ny){
     this.nx=nx;
     this.ny=ny;
-    this.data=math.zeros(ny,nx);
+    this.data=[];
+    for(var i=0;i<this.ny;i++){
+        this.data.push([]);
+        for(var j=0;j<this.nx;j++){
+            this.data[i].push(0.0);
+        }
+    }
+    this.set = function(i,j,v){
+        this.data[this.ny-j][i]=v;
+    }
+    this.get = function(i,j){
+        return this.data[this.ny-j][i];
+    }
 }
