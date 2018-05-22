2b57759b3a50aa068c36f3be32a6e860e99f4eb5
johnambrosiano <johnambrosiano@gmail.com>
2018-05-22 07:41:19 -0600

    Exploring math.js and oop.

--- oojs/grid.js
+++ oojs/grid.js
@@ -1,14 +1,8 @@
 /**
-    Defines a set of prototypes for a regular grid.
+    Defines a set of prototypes for regular grids.
 */
 function Grid(nx,ny){
     this.nx=nx;
     this.ny=ny;
-    this.data= [];
-    for(var i=0;i<this.ny;i++){
-        this.data.push([]);
-        for(var j=0;j<this.nx;j++){
-            this.data[i].push(0);
-        }
-    }
+    this.data=math.zeros(ny,nx);
 }
