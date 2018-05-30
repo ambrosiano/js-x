dc4875e60214d5dddc2bb3620c2767443c94f430
Ambrosiano <119751@win.lanl.gov>
2018-05-30 17:29:37 -0600

    Exploring sparql client in js.

--- node-x/sparql_client_graphdb_demo.js
+++ node-x/sparql_client_graphdb_demo.js
@@ -1,6 +1,6 @@
 var SparqlClient = require('sparql-client');
 var util = require('util');
-var endpoint = 'http://128.165.143.37:7200/repositories/cbd-tgr';
+var endpoint = 'http://highgarden.lanl.gov:7200/repositories/cbd-tgr/';
  
 // Get the leaderName(s) of the given citys
 // if you do not bind any city, it returns 10 random leaderNames
@@ -9,11 +9,14 @@ var endpoint = 'http://128.165.143.37:7200/repositories/cbd-tgr';
             'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n'+
             'select * where {?action rdf:type cbd:Action .}  limit 10';
             */
-var query = 'select * where {?s ?p ?o.}  limit 10';
+var query = 'select * where {?s ?p ?o}  limit 10';
 
 var client = new SparqlClient(endpoint);
 console.log("Query to " + endpoint);
 console.log("Query: " + query);
 client.query(query, function (error, results) {
-    process.stdout.write(util.inspect(arguments, null, 20, true) + "\n");
+    if(error){
+        console.log(error);
+    }
+    //process.stdout.write(util.inspect(arguments, null, 20, true) + "\n");
 });
